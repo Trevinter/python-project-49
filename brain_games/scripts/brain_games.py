0c2cdeaf4161216ab5d5e3a3e0522bc584145942
Kirill Babaev <babaevkv89@mail.ru>
2023-04-20 21:26:41 +0300

    update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,41 @@
 #!/usr/bin/env python3
+from brain_games.game_engine import start_game
 from brain_games import cli
+from brain_games.games import prime
+from brain_games.games import calc
+from brain_games.games import even
+from brain_games.games import gcd
+from brain_games.games import progression
 
 
 def welcome_user():
     cli.welcome_user()
 
 
+def game_prime():
+    start_game(prime)
+
+
+def game_calc():
+    start_game(calc)
+
+
+def game_even():
+    start_game(even)
+
+
+def game_gcd():
+    start_game(gcd)
+
+
+def game_progression():
+    start_game(progression)
+
+
 if __name__ == '__main__':
     welcome_user()
+    game_prime()
+    game_calc()
+    game_even()
+    game_gcd()
+    game_progression()
