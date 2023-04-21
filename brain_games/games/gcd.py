5397d7a09b61e25a30b1f7244e79c657a30117f8
Kirill Babaev <babaevkv89@mail.ru>
2023-04-21 21:07:44 +0300

    update gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,17 +1,21 @@
 from random import randint
 
 
-rules = 'Find the greatest common divisor of given numbers.'
+GAME_RULE = 'Find the greatest common divisor of given numbers.'
 
 
-def right_answer():
-    num1 = randint(3, 100)
-    num2 = randint(3, 100)
-    number = f'{str(num1)} {str(num2)}'
+def get_answer(num1, num2):
     while num1 != 0 and num2 != 0:
         if num1 > num2:
             num1 = num1 % num2
         else:
             num2 = num2 % num1
     sum = num1 + num2
-    return str(number), sum
+    return sum
+
+
+def get_game():
+    num1 = randint(3, 100)
+    num2 = randint(3, 100)
+    game_question = f'{str(num1)} {str(num2)}'
+    return str(game_question), get_answer(num1, num2)
