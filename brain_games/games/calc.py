2c7511f9c8425e2579295a08812b94e9a97be3e7
Kirill Babaev <babaevkv89@mail.ru>
2023-04-21 21:06:55 +0300

    update calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,20 +2,24 @@ from random import choice
 from random import randint
 
 
-rules = 'What is the result of the expression?'
+GAME_RULE = 'What is the result of the expression?'
 
 
-def right_answer():
+def get_answer(rand_operator, num1, num2):
+    if rand_operator == '+':
+        sum = int(num1) + int(num2)
+        return sum
+    elif rand_operator == '-':
+        sum = int(num1) - int(num2)
+        return sum
+    elif rand_operator == '*':
+        sum = int(num1) * int(num2)
+        return sum
+
+
+def get_game():
     rand_operator = choice(['+', '-', '*'])
     num1 = str(randint(1, 20))
     num2 = str(randint(1, 20))
-    number = f'{str(num1)} {rand_operator} {str(num2)}'
-    if rand_operator == '+':
-        sum = int(num1) + int(num2)
-        return str(number), sum
-    elif rand_operator == '-':
-        sum = int(num1) - int(num2)
-        return str(number), sum
-    elif rand_operator == '*':
-        sum = int(num1) * int(num2)
-        return str(number), sum
+    game_question = f'{str(num1)} {rand_operator} {str(num2)}'
+    return str(game_question), get_answer(rand_operator, num1, num2)
